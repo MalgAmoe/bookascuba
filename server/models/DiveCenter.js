91b3f5c8f0d640cb54294cfd004f75c19f6cb8e7
MalgAmoe <malgamoe@gmail.com>
2017-04-29 15:54:19 +0200

    feat: creating a DiveCenter model

--- server/models/DiveCenter.js
+++ server/models/DiveCenter.js
@@ -2,3 +2,26 @@
 
 const Sequelize = require('sequelize')
 const sequelize = require('../config/db.js')
+
+var DiveCenter = sequelize.define('diveCenter', {
+  username: {
+    type: Sequelize.STRING
+  },
+  email: {
+    type: Sequelize.STRING
+  },
+  password: {
+    type: Sequelize.STRING
+  },
+  name: {
+    type: Sequelize.STRING
+  },
+  location: {
+    type: Sequelize.STRING
+  },
+  description: {
+    type: Sequelize.STRING
+  }
+})
+
+module.exports = DiveCenter
