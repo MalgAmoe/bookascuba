c35ece101c8a3a7a8f087dfd05c5bb8e9b493bb2
MalgAmoe <malgamoe@gmail.com>
2017-04-29 19:08:48 +0200

    refactor: renaming stuff

--- server/models/DivingCenter.js
+++ server/models/DivingCenter.js
@@ -2,8 +2,8 @@
 
 const Sequelize = require('sequelize')
 const sequelize = require('../config/db.js')
-const user = require('./user')
-const event = require('./event')
+const user = require('./User')
+const event = require('./Event')
 
 var DivingCenter = sequelize.define('divingCenter', {
   location: {
