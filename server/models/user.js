1258d8daf1ed9013d38685fb764686e5b9670482
MalgAmoe <malgamoe@gmail.com>
2017-04-29 18:24:39 +0200

    feat: changed user model to include type and authorization level

--- server/models/user.js
+++ server/models/user.js
@@ -8,10 +8,16 @@ var User = sequelize.define('user', {
     type: Sequelize.STRING,
   },
   password: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
   },
   token: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+  },
+  type: {
+    type: Sequelize.STRING,
+  },
+  auth: {
+    type: Sequelize.INTEGER,
   }
 }, {
   freezeTableName: true // Model tableName will be the same as the model name
@@ -26,7 +32,9 @@ User.sync().then(function () {
       return User.create({
         username: username,
         password: '$2a$10$kgkM8ysm85eNHecJ4bdH8.yGzH0XE7HpfBmsit5JIqD6muFrxLr16',
-        token: '123456789'
+        token: '123456789',
+        type: 'dc',
+        auth: 1,
       });
     }
   })
